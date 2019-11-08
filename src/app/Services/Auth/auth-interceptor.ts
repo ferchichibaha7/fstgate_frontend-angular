47fc3c65ef6268847314e9fda8ccbcc641985138
Baha <ferchichibaha7@gmail.com>
2019-11-08 09:44:16 +0100

    test

--- src/app/Services/Auth/auth-interceptor.ts
+++ src/app/Services/Auth/auth-interceptor.ts
@@ -5,8 +5,6 @@ import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http'
 import { TokenStorageService } from './token-storage.service';
 
 const TOKEN_HEADER_KEY = 'Authorization';
-
-
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
